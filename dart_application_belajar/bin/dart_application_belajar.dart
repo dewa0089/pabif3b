7ea5762402339d5a1294ebceba0e00b10f1d7622
dewa0089 <newdewa03@gmail.com>
2023-10-03 12:22:04 +0700

    end

---
+++ dart_application_belajar/bin/dart_application_belajar.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_belajar/dart_application_belajar.dart' as dart_application_belajar;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_belajar.calculate()}!');
+}
